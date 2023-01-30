055348a6e8a3fac363dfa285c4ced49470a12a19
uriel <urieltesi@gmail.com>
2023-01-30 04:03:53 -0300

    commit trabajo final

---
+++ index.js
@@ -0,0 +1,24 @@
+    function accion1(){
+    var ancla = document.getElementsByClassName('exp');
+    for(var i =  0; i < ancla.length; i++){
+      ancla[i].classList.toggle('desaparece');
+    }
+    }
+    function accion4(){
+    var ancla = document.getElementsByClassName('cont');
+    for(var i =  0; i < ancla.length; i++){
+      ancla[i].classList.toggle('desaparece');
+    }
+    }
+function accion3(){
+    var ancla = document.getElementsByClassName('apt');
+    for(var i =  0; i < ancla.length; i++){
+      ancla[i].classList.toggle('desaparece');
+    }
+    }
+function accion2(){
+    var ancla = document.getElementsByClassName('forma');
+    for(var i =  0; i < ancla.length; i++){
+      ancla[i].classList.toggle('desaparece');
+    }
+    }
